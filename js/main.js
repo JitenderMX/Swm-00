0d313dfaf9c1cd45784d831a7c54cdd85af9b1f5
JitenderMX <jitender.work.mediax@gmail.com>
2024-04-08 17:27:20 +0530

    gallary image updated

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(window).on("load", function () {
             arrows: true,
             infinite: true,
             autoplay: true,
-            autoplaySpeed: 2000,
+            autoplaySpeed: 1500,
             centerMode: true,
             responsive: [
                 {
